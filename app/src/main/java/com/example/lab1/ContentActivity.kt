3227a37ab32f57fea63172549d6affbb905b6349
Александр <100239383+krikunAV@users.noreply.github.com>
2026-03-23 20:33:15 +0200

    add lab4 and lab5

--- app/src/main/java/com/example/lab1/ContentActivity.kt
+++ app/src/main/java/com/example/lab1/ContentActivity.kt
@@ -2,10 +2,27 @@ package com.example.lab1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ContentActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as androidx.navigation.fragment.NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
     }
 }
